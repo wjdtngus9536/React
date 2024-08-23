52cc2cec1560118695f8579c9f71658b098c062e
soohyun <wjdtngus9536@gmail.com>
2024-08-24 01:58:35 +0900

    update

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,42 +1,61 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function Header(props) {
-  console.log('props', props.title)
+  console.log(props)
   return (
     <header>
-    <h1><a href='/'>{props.title}</a></h1>
-  </header>
+      <h1>
+        <a href='/test' onClick={function (event) {
+          event.preventDefault();
+          props.onChangeMode();
+        }}>
+          {props.title}
+        </a>
+      </h1>
+    </header>
   )
 }
 
-function Nav() {
-  return(
+function Nav(props) {
+  // props.topics로 배열을 받으면 하나 씩 lis에 push후 return문에서 {lis}로 출력
+  const lis = []
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>);
+  }
+
+  return (
     <nav>
-        <ol>
-          <li><a href='/read/1'>html</a></li>
-          <li><a href='/read/2'>css</a></li>
-          <li><a href='/read/3'>js</a></li>
-        </ol>
-      </nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
   )
 }
 
-function Article() {
-  return(
+function Article(props) {
+  return (
     <article>
-    <h2>Welcome</h2>
-    Hello, WEB
-  </article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
   )
 }
 
 function App() {
+  const topics = [
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+    { id: 3, title: 'js', body: 'javascript is ...' }
+  ]
   return (
     <div>
-      <Header title="REACT"></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="WEB" onChangeMode={()=>{
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, Web"></Article>
     </div>
   );
 }
